dacba7cfab3160115d25f15ecbf65afcd25fb1d5
Felipe <fe.donatto@outlook.com>
2022-05-12 15:03:43 -0300

    requisito 8 finalizado

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,11 +19,11 @@ const myCounter = () => {
   var myArray = [];
   for (var counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (var counter2 = 2; counter2 <= 3; counter2 += 1) {
+      myArray.push(counter2);
     }
   }
   return myArray;
 };
-
+// nao sei se fiz certo, pareceu muito simples (?)
 module.exports = myCounter;
